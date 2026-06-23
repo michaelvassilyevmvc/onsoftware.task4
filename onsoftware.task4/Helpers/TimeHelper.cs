791b12abc3af841138c72093c64cfe53000a3478
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2026-06-23 16:27:32 +0500

    Helper to handle LastSeen status

---
+++ onsoftware.task4/Helpers/TimeHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace onsoftware.task4.Helpers;
+
+public static class TimeHelper
+{
+    public static string ToRelativeTime(DateTime dateTime)
+    {
+        var diff = DateTime.UtcNow - dateTime;
+
+        if (diff.TotalSeconds < 60)
+            return "less than a minute ago";  
+
+        if (diff.TotalMinutes < 60)
+            return $"{(int)diff.TotalMinutes} min ago";
+
+        if (diff.TotalHours < 24)
+            return $"{(int)diff.TotalHours} hours ago";
+
+        if (diff.TotalDays < 30)
+            return $"{(int)diff.TotalDays} days back";
+
+        if (diff.TotalDays < 365)
+            return $"{(int)(diff.TotalDays / 30)} months back";
+
+        return $"{(int)(diff.TotalDays / 365)} years ago";
+    }
+}

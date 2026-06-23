3b6b4bfdccec1f3e5552d40061f9f0b215a06a7f
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2026-06-23 16:33:47 +0500

    Added extensions for notifications and for working with users

---
+++ onsoftware.task4/Helpers/TempDataExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace onsoftware.task4.Helpers;
+
+public static class TempDataExtensions
+{
+    public static void Success(
+        this ITempDataDictionary tempData,
+        string message)
+    {
+        tempData["Success"] = message;
+    }
+
+    public static void Error(
+        this ITempDataDictionary tempData,
+        string message)
+    {
+        tempData["Error"] = message;
+    }
+
+    public static void Warning(
+        this ITempDataDictionary tempData,
+        string message)
+    {
+        tempData["Warning"] = message;
+    }
+
+    public static void Info(
+        this ITempDataDictionary tempData,
+        string message)
+    {
+        tempData["Info"] = message;
+    }
+}
---
+++ onsoftware.task4/Helpers/UserExtensions.cs
@@ -0,0 +1,40 @@
+﻿
+using onsoftware.task4.Areas.Identity.Data;
+
+namespace onsoftware.task4.Helpers;
+
+public static class UserExtensions
+{
+    public static IQueryable<ApplicationUser> Filter(
+        this IQueryable<ApplicationUser> query,
+        string? filter)
+    {
+        if (string.IsNullOrWhiteSpace(filter))
+            return query;
+
+        filter = filter.Trim();
+
+        return query.Where(x =>
+            x.FirstName!.Contains(filter) ||
+            x.LastName!.Contains(filter) ||
+            x.Email!.Contains(filter));
+    }
+    
+    public static IQueryable<ApplicationUser> Sort(
+        this IQueryable<ApplicationUser> query,
+        string? sort)
+    {
+        return sort switch
+        {
+            "email" => query.OrderBy(x => x.Email),
+
+            "email_desc" => query.OrderByDescending(x => x.Email),
+
+            "created" => query.OrderBy(x => x.CreatedAt),
+
+            "created_desc" => query.OrderByDescending(x => x.CreatedAt),
+
+            _ => query.OrderByDescending(x => x.UpdatedAt)
+        };
+    }
+}

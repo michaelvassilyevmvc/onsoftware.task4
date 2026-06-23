6257fd9a580fbf64f71e26f75cbbaa95894e073e
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2026-06-23 14:42:22 +0500

    Start project

---
+++ onsoftware.task4/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace onsoftware.task4.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
